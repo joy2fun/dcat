4c7bc65c5ce173340d963619fa412ab2cd93b154
chiao <php@html.js.cn>
2024-05-05 21:32:01 +0800

    fix jsoneditor height

--- resources/views/form/jsoneditor.blade.php
+++ resources/views/form/jsoneditor.blade.php
@@ -2,7 +2,7 @@
     <label class="{{$viewClass['label']}} control-label">{{$label}}</label>
     <div class="{{$viewClass['field']}}">
         @include('admin::form.error')
-        <div {!! $attributes !!} style="width: 100%; height: 100%;">
+        <div {!! $attributes !!} style="width: 100%;">
             <!-- <p>{!! $value !!}</p> -->
         </div>
         <input type="hidden" name="{{$name}}" value="{{ old($column, $value) }}" />
